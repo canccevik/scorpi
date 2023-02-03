f69dd4cb00bc8cde4f738342eaf987fc1fc766fa
Can Çevik <mazlumcancevik@gmail.com>
2023-02-03 04:01:20 +0300

    add http method decorators

---
+++ src/decorators/method/http-method.decorator.ts
@@ -0,0 +1,41 @@
+import { HttpMethod } from '@enums/index'
+import { Action } from '@interfaces/index'
+import { TypeMetadataStorage } from '@storages/index'
+
+const defaultName = '/'
+
+function createHttpMethodDecorator(
+  method: HttpMethod,
+  name: string | RegExp = defaultName
+): MethodDecorator {
+  return (target: object, propertyKey: string | symbol): void => {
+    const method = target[propertyKey as keyof typeof target]
+    const action: Action = { method, name }
+
+    TypeMetadataStorage.addActionMetadata({
+      target,
+      method,
+      action
+    })
+  }
+}
+
+export function Get(name?: string | RegExp): MethodDecorator {
+  return createHttpMethodDecorator(HttpMethod.GET, name)
+}
+
+export function Post(name?: string | RegExp): MethodDecorator {
+  return createHttpMethodDecorator(HttpMethod.POST, name)
+}
+
+export function Put(name?: string | RegExp): MethodDecorator {
+  return createHttpMethodDecorator(HttpMethod.PUT, name)
+}
+
+export function Patch(name?: string | RegExp): MethodDecorator {
+  return createHttpMethodDecorator(HttpMethod.PATCH, name)
+}
+
+export function Delete(name?: string | RegExp): MethodDecorator {
+  return createHttpMethodDecorator(HttpMethod.DELETE, name)
+}

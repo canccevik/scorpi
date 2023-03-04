8a2674d6f077fa3617ac4647751a5dba01523521
Can Çevik <mazlumcancevik@gmail.com>
2023-03-05 01:10:47 +0300

    refactor http method decorators

--- src/decorators/method/http-method.decorator.ts
+++ src/decorators/method/http-method.decorator.ts
@@ -1,32 +1,22 @@
 import { HttpMethod, HttpStatus } from '../../enums'
 import { createMethodDecorator } from '../../utils'
 
-const defaultName = '/'
-
-function createHttpMethodDecorator(
-  method: HttpMethod,
-  name: string | RegExp = defaultName,
-  statusCode = HttpStatus.OK
-): MethodDecorator {
-  return createMethodDecorator({ name, method, statusCode })
+export function Get(name: string | RegExp = '/'): MethodDecorator {
+  return createMethodDecorator({ method: HttpMethod.GET, name })
 }
 
-export function Get(name?: string | RegExp): MethodDecorator {
-  return createHttpMethodDecorator(HttpMethod.GET, name)
+export function Post(name: string | RegExp = '/'): MethodDecorator {
+  return createMethodDecorator({ method: HttpMethod.POST, statusCode: HttpStatus.CREATED, name })
 }
 
-export function Post(name?: string | RegExp): MethodDecorator {
-  return createHttpMethodDecorator(HttpMethod.POST, name, HttpStatus.CREATED)
+export function Put(name: string | RegExp = '/'): MethodDecorator {
+  return createMethodDecorator({ method: HttpMethod.PUT, name })
 }
 
-export function Put(name?: string | RegExp): MethodDecorator {
-  return createHttpMethodDecorator(HttpMethod.PUT, name)
+export function Patch(name: string | RegExp = '/'): MethodDecorator {
+  return createMethodDecorator({ method: HttpMethod.PATCH, name })
 }
 
-export function Patch(name?: string | RegExp): MethodDecorator {
-  return createHttpMethodDecorator(HttpMethod.PATCH, name)
-}
-
-export function Delete(name?: string | RegExp): MethodDecorator {
-  return createHttpMethodDecorator(HttpMethod.DELETE, name)
+export function Delete(name: string | RegExp = '/'): MethodDecorator {
+  return createMethodDecorator({ method: HttpMethod.DELETE, name })
 }

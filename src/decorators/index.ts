1fb71ed8fc07dc240b879efc53516d208c240b86
Can Çevik <mazlumcancevik@gmail.com>
2023-03-17 11:52:52 +0300

    add req/request decorators

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,3 +1,4 @@
 export * from './class'
 export * from './method'
 export * from './common'
+export * from './param'
---
+++ src/decorators/param/request.decorator.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator } from './create-param-decorator'
+
+export function Request(): Function {
+  return createParamDecorator('req', (req) => req)
+}
+
+export function Req(): Function {
+  return createParamDecorator('req', (req) => req)
+}
